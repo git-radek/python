f3d390b0de279d49bad1d5fa3040135556c77d35
git-radek <57450213+git-radek@users.noreply.github.com>
2020-11-24 23:34:42 +0100

    Simple pulse generator v0.1 for pigpiod library

---
+++ simple_pulse_generator.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+#2020/11/24 Radosław Michoń - Simple pulse generator v0.1
+
+import time
+import pigpio
+
+port=16
+dc=64
+
+pi = pigpio.pi()
+pi.set_PWM_dutycycle(port,dc)
+
+if not pi.connected:
+   exit()
+
+print("Sending pulses to GPIO "+str(port)+", control+C to stop.")
+
+while True:
+   try:
+      speed=100
+      speed=(speed*12)
+      pi.set_PWM_frequency(port,speed)
+      time.sleep(1)
+
+   except KeyboardInterrupt:
+       print("\nTidying up")
+       pi.set_PWM_frequency(port,0)
+       break
+
+pi.stop()
